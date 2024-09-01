f947fd255bcf89f396f51d12ad23043a73eb80dd
Ngwere <ngwerecliford@gmail.com>
2024-09-01 15:21:49 +0000

    changed 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,13 +1,17 @@
-#!/usr/bin/python3
-""" BaseCaching module
+#!/usr/bin/env python3
+"""Module for task 1.
 """
 from collections import OrderedDict
+
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """A caching system that inherits from BaseCaching and does not have a
-    limit on the number of items it can store.
+    """A caching system that inherits from BaseCaching and uses a FIFO
+    algorithm to discard items.
+
+    Args:
+        BaseCaching (_type_): _description_
     """
     def __init__(self):
         """Initializes the cache and calls the parent class's init method.
@@ -18,9 +22,9 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """Assigns the item value to the key in the dictionary self.cache_data.
         If key or item is None, this method should not do anything.
-        If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
-           must discard the first item put in cache (FIFO algorithm)
-           must print DISCARD: with the key discarded and following by a new line
+        If the number of items in self.cache_data is higher than
+        BaseCaching.MAX_ITEMS, the oldest item is discarded using a FIFO
+        algorithm and a message is printed.
 
         Args:
             key (any): param1.
@@ -28,17 +32,12 @@ class FIFOCache(BaseCaching):
         """
         if key is None or item is None:
             return
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key = next(iter(self.cache_data))
-            print("Discared: {}".format(first_key))
         self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
 
     def get(self, key):
-        """Returns the value in self.cache_data linked to key.
-        If key is None or if the key doesn't exist in self.cache_data,
-        return None.
-
-        Args:
-            key (any): param1.
+        """Retrieves an item by key.
         """
         return self.cache_data.get(key, None)
